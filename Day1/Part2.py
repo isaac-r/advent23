8d204f587ef1b0cec36e10da1fd4d0e6b3417469
isaac.richardson <isaacr828@gmail.com>
2023-12-03 13:25:18 +0000

    completeing task 2

--- Day1/Part2.py
+++ Day1/Part2.py
@@ -1,39 +1,47 @@
 from operator import itemgetter
+
+def readNumberString(line):
+  numberStrings = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  returnNumberStringList = []
+  for string in numberStrings:
+    startTest = line.find(string)
+    endTest = line.rfind(string)
+    if (startTest > -1):
+     stringNumberList = [numberStrings.index(string) + 1, startTest]
+     returnNumberStringList.append(stringNumberList)
+    if (endTest > -1):
+     stringNumberList2 = [numberStrings.index(string) + 1, endTest]
+     returnNumberStringList.append(stringNumberList2)
+
+  return returnNumberStringList
+
+def readNumberCharacter(line, numberString):
+  index = -1
+  for character in line:
+    index = index + 1
+    if (character.isnumeric()):
+      characterNumberList = [int(character), index]
+      numberString.append(characterNumberList)
+
 f = open("input.txt", "r")
-# Run through each line
 totalValue = 0
-numberStrings = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
 for line in f:
   print(line)
-  numbers = []
 
-  for string in numberStrings:
-    if string in line:
-      #print(numberStrings.index(string) + 1, line.index(string))
-      stringNumberList = [numberStrings.index(string) + 1, line.index(string)]
-      #print(stringNumberList)
-      #newNumbers.insert(numberStrings.index(string) + 1, line.index(string))
-      numbers.append(stringNumberList)
+  numberString = readNumberString(line)
 
-  for character in line:    
-    if (character.isnumeric()):
-      #print(character, line.index(character))
-      characterNumberList = [int(character), line.index(character)]
-      #print(characterNumberList)
-      numbers.append(characterNumberList)
-  print(numbers)
+  readNumberCharacter(line, numberString)
 
-  sortedList = sorted(numbers, key=itemgetter(1))
-
-  requiredVals = [sortedList[0][0], sortedList[len(sortedList) - 1][0]]
-
-  print(requiredVals)
+  sortedList = sorted(numberString, key=itemgetter(1))
 
   print(sortedList)
-  #value = numbers[0] + numbers[len(numbers) - 1]
-  #print(value)
-  #totalValue = totalValue + int(value)
-  #print(numbers[0] + numbers[len(numbers) - 1])
+
+  requiredVals = [sortedList[0][0], sortedList[len(sortedList) - 1][0]]
+  addedVals = str(sortedList[0][0]) + str(sortedList[len(sortedList) - 1][0])
+  print(addedVals)
+
+  totalValue = totalValue + int(addedVals)
 
 print(totalValue)
 f.close()
